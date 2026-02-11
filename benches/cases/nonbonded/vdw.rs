5f1141a570fc129b68aca73b1edaa9cfcff8cbce
Tony Kan <tonykan@caltech.edu>
2026-02-10 21:06:07 -0800

    test(benches): Update Buckingham potential parameters

--- benches/cases/nonbonded/vdw.rs
+++ benches/cases/nonbonded/vdw.rs
@@ -1,6 +1,6 @@
 use crate::bench_pair_potential;
 use criterion::Criterion;
-use dreid_kernel::potentials::nonbonded::{Buckingham, LennardJones, SplinedBuckingham};
+use dreid_kernel::potentials::nonbonded::{Buckingham, LennardJones};
 
 pub fn bench_vdw(c: &mut Criterion) {
     bench_pair_potential!(c, "non-bonded/lennard-jones", LennardJones, (1.0, 4.0), 4.0);
@@ -9,7 +9,13 @@ pub fn bench_vdw(c: &mut Criterion) {
         c,
         "non-bonded/buckingham",
         Buckingham,
-        (162754.79141900392, 6.0, 128.0, 0.25),
+        (
+            162754.79141900392,
+            6.0,
+            128.0,
+            0.3659637525866052,
+            3410.825177656595
+        ),
         4.0
     );
 }
